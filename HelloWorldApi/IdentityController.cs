182ed7da7617606c19df97f587358fc4d73cffe0
Oleh Stebliev <oleh.stebliev@globallogic.com>
2025-03-13 12:04:39 +0200

    identity server init

---
+++ HelloWorldApi/IdentityController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldApi;
+
+[Route("identity")]
+[ApiController]
+public class IdentityController : ControllerBase
+{
+    private readonly ILogger<IdentityController> _logger;
+
+    public IdentityController(ILogger<IdentityController> logger)
+    {
+        _logger = logger;
+    }
+
+    // this action simply echoes the claims back to the client
+    [HttpGet]
+    public ActionResult Get()
+    {
+        var claims = User.Claims.Select(c => new { c.Type, c.Value });
+        _logger.LogInformation("claims: {claims}", claims);
+
+        return new JsonResult(claims);
+    }
+}
