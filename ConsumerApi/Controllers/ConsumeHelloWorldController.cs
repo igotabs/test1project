544d9ec3158badf0a89cb8eb7a10a9e202edb1cd
oleg steblev <oleh.stebliev@globallogic.com>
2025-03-24 23:56:47 +0200

    fix

--- ConsumerApi/Controllers/ConsumeHelloWorldController.cs
+++ ConsumerApi/Controllers/ConsumeHelloWorldController.cs
@@ -18,7 +18,6 @@ using Polly.Retry;
 
 namespace ConsumerApi.Controllers
 {
-
 	// Thread-safe random generator
 	static class RandomInterval
 	{
@@ -36,56 +35,68 @@ namespace ConsumerApi.Controllers
 
 
 	[ApiController]
-    [Route("[controller]")]
-    public class ConsumeHelloWorldController : ControllerBase
-    {
-        private readonly string? _identityServerBaseUrl;
-        private readonly IHelloWorldTokenService _tokenService;
-        private readonly HelloWorldApiClient _helloWorldApiClient;
-        private readonly string? _helloWorldApiBaseUrl;
+	[Route("[controller]")]
+	public class ConsumeHelloWorldController : ControllerBase
+	{
+		private readonly string? _identityServerBaseUrl;
+		private readonly IHelloWorldTokenService _tokenService;
+		private readonly HelloWorldApiClient _helloWorldApiClient;
+		private readonly string? _helloWorldApiBaseUrl;
 
-        public ConsumeHelloWorldController(
-            IConfiguration configuration,
-            IHelloWorldTokenService tokenService,
-            HelloWorldApiClient helloWorldApiClient)
-        {
-            _identityServerBaseUrl = configuration["IdentityServer:BaseUrl"] ?? throw new ArgumentNullException(nameof(_identityServerBaseUrl));
-            _helloWorldApiBaseUrl = configuration["HelloWorldApi:BaseUrl"] ?? throw new ArgumentNullException(nameof(_helloWorldApiBaseUrl));
-            _tokenService = tokenService;
-            _helloWorldApiClient = helloWorldApiClient;
-        }
+		public ConsumeHelloWorldController(
+			IConfiguration configuration,
+			IHelloWorldTokenService tokenService,
+			HelloWorldApiClient helloWorldApiClient)
+		{
+			_identityServerBaseUrl = configuration["IdentityServer:BaseUrl"] ??
+			                         throw new ArgumentNullException(nameof(_identityServerBaseUrl));
+			_helloWorldApiBaseUrl = configuration["HelloWorldApi:BaseUrl"] ??
+			                        throw new ArgumentNullException(nameof(_helloWorldApiBaseUrl));
+			_tokenService = tokenService;
+			_helloWorldApiClient = helloWorldApiClient;
+		}
 
-        [HttpGet("{count}", Name = "GetHelloWorld")]
-        public async Task<IEnumerable<HelloWorld?>> Get(int count = 1)
-        {
-	        AsyncRetryPolicy<string> tokenRetryPolicy = Policy<string>
+		[HttpGet("{count}", Name = "GetHelloWorld")]
+		public async Task<ActionResult<IEnumerable<HelloWorld?>>> Get(int count = 1)
+		{
+			AsyncRetryPolicy<string> tokenRetryPolicy = Policy<string>
 				.Handle<Exception>()
-		        .OrResult(string.IsNullOrEmpty)
+				.OrResult(string.IsNullOrEmpty)
 				.WaitAndRetryAsync(3,
-			        retryAttempt => RandomInterval.GetRandomDelay(1, 2)); // 1-3 seconds
+					retryAttempt => RandomInterval.GetRandomDelay(1, 2)); // 1-3 seconds
 
-	        AsyncRetryPolicy<HelloWorld?> serviceRetryPolicy = Policy<HelloWorld?>
-		        .Handle<Exception>()
-		        .OrResult(r => r == null)
-		        .WaitAndRetryAsync(3,
-			        retryAttempt => RandomInterval.GetRandomDelay(1, 2)); // 2-5 seconds
+			AsyncRetryPolicy<HelloWorld?> serviceRetryPolicy = Policy<HelloWorld?>
+				.Handle<Exception>()
+				.OrResult(r => r == null)
+				.WaitAndRetryAsync(3,
+					retryAttempt => RandomInterval.GetRandomDelay(1, 2)); // 2-5 seconds
 
-
-
-
-	        var token = await _tokenService.GetAccessTokenAsync();
-	        _helloWorldApiClient.Client.SetBearerToken(token);
-	        var results = new ConcurrentBag<HelloWorld>();
-	        var errors = new ConcurrentBag<(HttpStatusCode StatusCode, string ErrorMessage)>();
+			var token = await _tokenService.GetAccessTokenAsync();
+			_helloWorldApiClient.Client.SetBearerToken(token);
+			var results = new ConcurrentBag<HelloWorld>();
+			var errors = new ConcurrentBag<(HttpStatusCode StatusCode, string ErrorMessage)>();
 
 			await Parallel.ForEachAsync(
-                Enumerable.Range(1, count),
-                async (index, cancellationToken) =>
-                {
-	                var item = await serviceRetryPolicy.ExecuteAsync(() => CallServiceAsync(token));
-                    if (item != null) results.Add(item);
-                }
-            );
+				Enumerable.Range(1, count),
+				async (index, cancellationToken) =>
+				{
+					try
+					{
+						var item = await serviceRetryPolicy.ExecuteAsync(() => CallServiceAsync());
+						if (item != null) results.Add(item);
+					}
+					catch (HttpRequestException ex) when (ex.StatusCode.HasValue)
+					{
+						errors.Add((ex.StatusCode.Value, ex.Message));
+					}
+					catch (Exception ex)
+					{
+						errors.Add((HttpStatusCode.InternalServerError, ex.Message));
+					}
+				}
+			);
+			if (errors.IsEmpty)
+				return results.ToList();
 
 			var errorDetails = errors.Select(e => $"Status {e.StatusCode}: {e.ErrorMessage}");
 			return StatusCode(500, new
@@ -94,8 +105,8 @@ namespace ConsumerApi.Controllers
 				Results = results.ToList(),
 				Errors = errorDetails
 			});
-
 		}
+
 		async Task<HelloWorld?> CallServiceAsync()
 		{
 			try
